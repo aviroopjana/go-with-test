c6d300e6c15e265556b9d185e495b71d3c00aa38
Aviroop Jana <aviroopjana@gmail.com>
2024-01-17 22:09:53 +0530

    Refactor and Update the program and along with it's test cases

--- Arrays_Slices/sum_test.go
+++ Arrays_Slices/sum_test.go
@@ -3,13 +3,27 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := []int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	expected := 16
+	t.Run("Collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	if got != expected {
-		t.Errorf("got %d expected %d, given %v", got, expected, numbers)
-	}
+		got := Sum(numbers)
+		expected := 15
+
+		if got != expected {
+			t.Errorf("got %d expected %d, given %v", got, expected, numbers)
+		}
+	})
+
+	t.Run("Collection of any number", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		expected := 6
+
+		if got != expected {
+			t.Errorf("got %d expected %d, given %v", got, expected, numbers)
+		}
+	})
 
 }
