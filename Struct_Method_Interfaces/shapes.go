916cf044f0dad9860249ca78cdecb0abe7a4cc1a
Aviroop Jana <aviroopjana@gmail.com>
2024-01-19 02:20:31 +0530

    Refactor using struct and interface alse added new triangle case

--- Struct_Method_Interfaces/shapes.go
+++ Struct_Method_Interfaces/shapes.go
@@ -2,6 +2,10 @@ package main
 
 import "math"
 
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	length  float64
 	breadth float64
@@ -11,10 +15,23 @@ type Circle struct {
 	radius float64
 }
 
+type Triangle struct {
+	height float64
+	base   float64
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.length + rectangle.breadth)
+}
+
 func (r Rectangle) Area() float64 {
 	return r.length * r.breadth
 }
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.radius * c.radius
+}
+
+func (t Triangle) Area() float64 {
+	return (t.base * t.height * 0.5)
 }
--- Struct_Method_Interfaces/shapes_test.go
+++ Struct_Method_Interfaces/shapes_test.go
@@ -2,24 +2,37 @@ package main
 
 import "testing"
 
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %g want %g", got, want)
+	}
+}
+
 func TestArea(t *testing.T) {
-	t.Run("Rectangle", func(t *testing.T) {
-		rectangle := Rectangle{20, 20}
-		got := rectangle.Area()
-		expected := 400.0
 
-		if got != expected {
-			t.Errorf("got %g expected %g", got, expected)
-		}
-	})
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{length: 12, breadth: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{height: 12, base: 6}, hasArea: 36.0},
+	}
 
-	t.Run("Circle", func(t *testing.T) {
-		circle := Circle{10}
-		got := circle.Area()
-		expected := 314.1592653589793
+	for _, tt := range areaTests {
+		// using tt.name from the case to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
 
-		if got != expected {
-			t.Errorf("got %g expected %g", got, expected)
-		}
-	})
+	}
+
 }
